2d02184fd04acff12b9237dae7d0a3a5313afc8c
m1000 <74680137+xParzivalx@users.noreply.github.com>
2023-09-10 12:32:11 +0200

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -22,9 +22,10 @@ export async function POST(req: Request) {
         role: 'user',
         content: `Generate 1 ${vibe} text with Mardown format and clearly with spacings and headers. ${
           vibe === 'Persuasive'
-            ? "Make sure you are using Markdown style for headings and important words in *bold*."
+            ? "Make sure you are writing persuasive, that you sell with maximum persuasion and use of word."
             : null
         }
+          Make sure you are using Markdown style for ##headings and important words in *bold*.
           Make sure each generated texts are at least 1000 characters, based on this context or title: ${bio}${
           bio.slice(-1) === '.' ? '' : '.'
         }`,
